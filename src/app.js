ecb566df5d272183430d2c3e0329e2d42ca5b03c
Jayraj-parki <jayrajfd@gmail.com>
2021-09-10 12:30:13 +0530

    added correct url path

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 require('dotenv').config({path:"./config.env"})
 const PORT=process.env.PORT ||5000
-// const cookieparser=require("cookie-parser")
+const path=require('path')
 const app=express()
 require("./db/conn")
 
@@ -9,6 +9,13 @@ app.use(express.json())
 // app.use(cookieparser())
  
 app.use(require('./router/routes'))
+
+app.use(express.static(path.join(__dirname,"../client/build")));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname,"../client/build", "index.html"));
+  });
+
 if(process.env.NODE_ENV=="production"){
     app.use(express.static("client/build"))
 }
